9c40af14d49cf08775b3344432e0f20c9466dcdb
Anggih Septiawan <albertanggih@gmail.com>
2020-05-01 14:08:41 +0700

    change model spinner and change background body

--- src/components/spinner/spinner.component.jsx
+++ src/components/spinner/spinner.component.jsx
@@ -1,8 +1,14 @@
-import React from 'react';
-import './spinner.style.scss';
+import React from "react";
+import "./spinner.style.scss";
 
 const Spinner = () => (
-    <div className="spinner"></div>
+	<div className="spinner">
+		<div class="lds-ring">
+			<div></div>
+			<div></div>
+			<div></div>
+		</div>
+	</div>
 );
 
 export default Spinner;
