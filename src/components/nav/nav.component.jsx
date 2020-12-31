1cd85faf25d84c13abb3636a81a2fa8ccc6ab6bf
Anggih Septiawan <albertanggih@gmail.com>
2021-01-01 01:03:42 +0700

    add fixed navbar on scroll and change to functional component

--- src/components/nav/nav.component.jsx
+++ src/components/nav/nav.component.jsx
@@ -1,72 +1,72 @@
-import React from "react";
+import React, { useState, useRef } from "react";
 import "./nav.style.scss";
 import { Link } from "react-router-dom";
 import Fade from "react-reveal/Fade";
 import Zoom from "react-reveal/Zoom";
 
-class Nav extends React.Component {
-	constructor() {
-		super();
+const Nav = () => {
+	const [toggleHidden, setToggleHidden] = useState(true);
+	const nav = useRef(null);
 
-		this.state = {
-			toggleHidden: true,
-		};
-	}
-
-	handleNav = () => {
-		this.setState({ toggleHidden: !this.state.toggleHidden });
+	const handleNav = () => {
+		setToggleHidden(!toggleHidden);
 	};
 
-	render() {
-		return (
-			<div className="container">
-				<div className="navbar">
-					<Fade delay="1500">
-						<div className="brand">
-							<span className="text-teal">asdevv.</span>
-						</div>
-						<div
-							className={
-								this.state.toggleHidden === true
-									? "toggle"
-									: "toggle active"
-							}
-							onClick={this.handleNav}
-						>
-							<span></span>
-							<span></span>
-							<span></span>
-						</div>
-					</Fade>
+	window.addEventListener("scroll", () => {
+		if (window.pageYOffset > 10) {
+			if (nav.current !== null) {
+				nav.current.classList.add("fixed");
+			}
+		} else {
+			if (nav.current !== null) {
+				nav.current.classList.remove("fixed");
+			}
+		}
+	});
 
-					{this.state.toggleHidden === true ? null : (
-						<Zoom>
-							<div className="link">
-								<Fade bottom delay="500">
-									<Link to="/" onClick={this.handleNav}>
-										HOME
-									</Link>
-								</Fade>
-								<Fade bottom delay="800">
-									<Link to="/about" onClick={this.handleNav}>
-										ABOUT
-									</Link>
-								</Fade>
-								<Fade bottom delay="1000">
-									<Link
-										to="/contact"
-										onClick={this.handleNav}
-									>
-										CONTACT
-									</Link>
-								</Fade>
-							</div>
-						</Zoom>
-					)}
-				</div>
+	return (
+		<div className="navbar" ref={nav}>
+			<div className="container">
+				<Fade delay="1500">
+					<div className="brand">
+						<span className="text-teal">asdevv.</span>
+					</div>
+					<div
+						className={
+							toggleHidden === true ? "toggle" : "toggle active"
+						}
+						onClick={handleNav}
+					>
+						<span></span>
+						<span></span>
+						<span></span>
+					</div>
+				</Fade>
 			</div>
-		);
-	}
-}
+
+			{toggleHidden === true ? null : (
+				<Zoom>
+					<div className="link">
+						<Fade bottom delay="500">
+							<Link to="/" onClick={handleNav}>
+								HOME
+							</Link>
+						</Fade>
+						<Fade bottom delay="800">
+							<Link to="/about" onClick={handleNav}>
+								ABOUT
+							</Link>
+						</Fade>
+						<Fade bottom delay="1000">
+							<Link to="/contact" onClick={handleNav}>
+								CONTACT
+							</Link>
+						</Fade>
+					</div>
+				</Zoom>
+			)}
+		</div>
+	);
+};
 
 export default Nav;
